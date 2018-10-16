04c18c74a99729dd076a6398c54eb13099551b15
madara125 <martinortega@ciencias.unam.mx>
2018-10-15 19:21:30 -0500

    agregados archivos para el evio de correos

--- Biologger/target/generated-sources/annotations/com/biologger/usuario/modelo/Usuario_.java
+++ Biologger/target/generated-sources/annotations/com/biologger/usuario/modelo/Usuario_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2018-10-15T14:55:08")
+@Generated(value="EclipseLink-2.7.3.v20180807-rNA", date="2018-10-15T19:19:04")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
