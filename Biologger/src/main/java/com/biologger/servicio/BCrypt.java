67b33f14cfcebb5e9fc75a206a091051b0a92c79
Alex Aldaco <alexa@MacBook-Pro-de-Alex.local>
2018-10-20 08:45:57 -0500

    Reestructuración clases de servicio

--- Biologger/src/main/java/com/biologger/servicio/BCrypt.java
+++ Biologger/src/main/java/com/biologger/servicio/BCrypt.java
@@ -12,7 +12,7 @@
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-package com.biologger.cifrado;
+package com.biologger.servicio;
 import java.io.UnsupportedEncodingException;
 import java.security.SecureRandom;
 
--- Biologger/src/main/java/com/biologger/validaciones/ValidacionSubidaImagenes.java
+++ Biologger/src/main/java/com/biologger/validaciones/ValidacionSubidaImagenes.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.biologger.usuario.controlador;
+package com.biologger.validaciones;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
@@ -18,28 +18,25 @@ import javax.servlet.http.Part;
  * @author alexa
  */
 @FacesValidator(value="UploadImageValidator")
-public class UploadImageValidator implements Validator{
+public class ValidacionSubidaImagenes implements Validator{
  
     @Override
     public void validate(FacesContext context, UIComponent component, Object value)     
             throws ValidatorException {
-        
         Part file = (Part) value;
         FacesMessage message=null;
- 
         try {
             if (file.getSize()>524288) {
-                message=new FacesMessage("Archivo demasiado grande. El máximo permitido para la foto es un peso de 512KB.");
+                message=new FacesMessage("Archivo demasiado grande. "
+                        + "El máximo permitido para la foto es un peso de 512KB.");
             }
  
-            if (message!=null && !message.getDetail().isEmpty())
-                {
-                    message.setSeverity(FacesMessage.SEVERITY_ERROR);
-                    throw new ValidatorException(message );
-                }
- 
+            if (message!=null && !message.getDetail().isEmpty()) {
+                message.setSeverity(FacesMessage.SEVERITY_ERROR);
+                throw new ValidatorException(message );
+            }
         } catch (Exception ex) {
-               throw new ValidatorException(new FacesMessage(ex.getMessage()));
+            throw new ValidatorException(new FacesMessage(ex.getMessage()));
         }
  
     }
